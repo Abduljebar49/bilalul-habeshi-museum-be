e7425a74d5f7ad4c889835be470f1ac5315606f7
Abduljebar <57292618+Abduljebar49@users.noreply.github.com>
2022-08-15 10:00:57 +0300

    database connection url changed

--- connection.js
+++ connection.js
@@ -2,8 +2,8 @@ let mysql = require("mysql");
 
 let connection = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "",
+  user: "skylinkictcom_bilalhabeshiuser",
+  password: "jA5bzRsUI(^m",
   database: "skylinkictcom_bilal-habeshi",
 });
 
