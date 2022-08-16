c5020ee43a4b3ee6cfedfde5d4a5ae2be581df3b
Abduljebar <57292618+Abduljebar49@users.noreply.github.com>
2022-08-16 14:18:02 +0300

    photobase upload edited

--- routes/photo-based-route.js
+++ routes/photo-based-route.js
@@ -14,7 +14,7 @@ router.post("/pb", upload.single("image"), create);
 router.get("/pb/:id", getSingleData);
 router.delete("/pb/:id", deletePhotoBased);
 router.get("/pb", getAll);
-router.patch('/pb/:id',update);
+router.patch('/pb/:id',upload.single("image"),update);
 
 module.exports = {
   routes: router,
