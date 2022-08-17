ef27e0acbc3aad5a314a0276aaa002e22c139324
Abduljebar <57292618+Abduljebar49@users.noreply.github.com>
2022-08-17 19:20:09 +0300

    category edit

--- routes/category-route.js
+++ routes/category-route.js
@@ -4,16 +4,17 @@ const {
   getAllCategory,
   addCategory,
   deleteCategory,
+  updateCategory,
 } = require("../controllers/category-controller");
 
 const router = express.Router();
 
 router.get("/category/:id", getCategory);
+router.patch("/category/:id", updateCategory);
 router.get("/category", getAllCategory);
 router.post("/category", addCategory);
 router.delete("/category/:id", deleteCategory);
 
-
 module.exports = {
-    routes:router
-}
+  routes: router,
+};
