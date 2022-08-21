9738a603c4ca2fbbb28abbb099818e58cd894444
Abdurahman Jafer <95960560+its0beno@users.noreply.github.com>
2022-08-21 15:54:02 +0300

    deleted a space

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const corsOpts = {
 
 app.use(cors(corsOpts));
 // app.use(cors());
-app.use(express.static("./public"));
+app.use(express.static("./public")); 
 // app.use(bodyparser.json());
 app.use(
   bodyparser.urlencoded({
