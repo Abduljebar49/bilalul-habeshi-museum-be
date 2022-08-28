0d64f201312a7793d2e49266b61a59de2e9b51bf
Abduljebar <57292618+Abduljebar49@users.noreply.github.com>
2022-08-28 07:25:36 +0300

    index chane

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const userRoute = require("./routes/user-route");
 const roleRoute = require("./routes/role-route");
 const categoryRoute = require("./routes/category-route");
 const photoBasedRoute = require("./routes/photo-based-route");
+// const videoBasedRoute = require("./routes/video-based-route");
 const app = express();
 const http = require("http");
 const db = require("./connection");
@@ -32,14 +33,22 @@ app.use(express.json());
 app.use("/api", userRoute.routes);
 app.use("/api", roleRoute.routes);
 app.use("/api", photoBasedRoute.routes);
+app.use("/api", videoBasedRoute.routes);
 app.use("/api", categoryRoute.routes);
-app.use("/api", categoryRoute.routes);
-// app.post("/post", upload.single("image"), (req, res) => {
+// app.use("/api", categoryRoute.routes);
 
-// });
- 
 // app.listen(3000, function () {
 //   console.log("listening on port 3000 ");
 // });
+
+// var http = require('http');
+// var server = http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     var message = 'It works!\n',
+//         version = 'NodeJS ' + process.versions.node + '\n',
+//         response = [message, version].join('\n');
+//     res.end(response);
+// });
+// server.listen();
 var server = http.createServer(app);
 server.listen();
